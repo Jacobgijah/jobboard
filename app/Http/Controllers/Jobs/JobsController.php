3161ec3f70a22a251e6840536927fd722b678329
Jacobgijah <jacobgijjah@gmail.com>
2024-01-29 08:57:32 +0300

    initial: save jobs

---
+++ app/Http/Controllers/Jobs/JobsController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Jobs;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Job\Job;
+use App\Models\Job\JobSaved;
+use Illuminate\Support\Facades\Auth;
+
+class JobsController extends Controller
+{
+    
+    public function single($id)
+    {
+        $job = Job::find($id); // get a single job by id
+
+        // getting related jobs
+        $relatedJobs = Job::where('category', $job->category)
+            ->where('id', '!=', $id)
+            ->take(5)
+            ->get();
+
+        $relatedJobsCount = Job::where('category', $job->category)
+            ->where('id', '!=', $id)
+            ->take(5)
+            ->count();
+
+        // save jobs
+        $savedJob = JobSaved::where('job_id', $id)
+            ->where('user_id', Auth::user()->id)
+            ->count();
+
+        return view('jobs.single', compact('job', 'relatedJobs', 'relatedJobsCount', 'savedJob'));
+    }
+
+    public function saveJob(Request $request)
+    {
+        $saveJob = JobSaved::create([
+            'job_id' => $request->job_id,
+            'user_id' => $request->user_id,
+            'job_image' => $request->job_image,
+            'job_title' => $request->job_title,
+            'job_region' => $request->job_region,
+            'job_type' => $request->job_type,
+            'company' => $request->company,
+        ]);
+
+        if($saveJob)
+        {
+            return redirect('/jobs/single/'.$request->job_id.'')->with('save', 'job saved successfully');
+        }
+    }
+
+}
---
+++ app/Models/Job/Job.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\Job;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $table = 'jobs';
+
+    protected $fillable = [
+        'id',
+        'job_title',
+        'company',
+        'job_region',
+        'job_type',
+        'vacancy',
+        'experience',
+        'salary',
+        'gender',
+        'application_deadline',
+        'job_description',
+        'responsibilities',
+        'education_experiences',
+        'other_benefits',
+        'image',
+        'category',
+    ];
+
+    public $timestamps = true;
+}
